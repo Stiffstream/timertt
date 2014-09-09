5ecb8f7a07aa19726dfae8d010e3cdf3f8a07377
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2014-09-09 10:59:08 +0000

    Fix compilation error on Linux
    git-svn-id: https://svn.code.sf.net/p/sobjectizer/repo/branches/timertt/1.0@846 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/build.rb
+++ dev/build.rb
@@ -4,6 +4,10 @@ MxxRu::Cpp::composite_target( MxxRu::BUILD_ROOT ) {
 
 	toolset.force_cpp0x_std
 	global_include_path "."
+	
+	if 'gcc' == toolset.name
+		global_linker_option '-pthread'
+	end
 
 	# If there is local options file then use it.
 	if FileTest.exist?( "local-build.rb" )
